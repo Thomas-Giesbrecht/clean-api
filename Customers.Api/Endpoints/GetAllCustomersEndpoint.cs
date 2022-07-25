e50634a3681843ccc93e4d423a93081ed113a3f9
Thomas Giesbrecht <thomas.giesbrecht@outlook.de>
2022-07-25 23:28:43 +0200

    add getallcustomers endpoint; add swagger ui

---
+++ Customers.Api/Endpoints/GetAllCustomersEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using Customers.Api.Contracts.Responses;
+using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Customers.Api.Endpoints;
+
+[HttpGet("customers"), AllowAnonymous]
+public class GetAllCustomersEndpoint : EndpointWithoutRequest<GetAllCustomersResponse>
+{
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var customer = new CustomerResponse()
+        {
+            Id = Guid.NewGuid(),
+            Username = "Thomas-Giesbrecht",
+            FullName = "Thomas Giesbrecht",
+            Email = "mail@gmail.com",
+            DateOfBirth = new DateTime(2022, 7, 25)
+        };
+
+        var response = new GetAllCustomersResponse
+        {
+            Customers = new List<CustomerResponse> {customer}
+        };
+        
+        await SendOkAsync(response, ct);
+    }
+}
