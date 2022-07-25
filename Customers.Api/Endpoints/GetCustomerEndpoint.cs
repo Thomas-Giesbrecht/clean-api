0d6acfa93f50ec596333846f10a250f516752b4b
Thomas Giesbrecht <thomas.giesbrecht@outlook.de>
2022-07-26 00:04:39 +0200

    Create GetCustomerEndpoint.cs

---
+++ Customers.Api/Endpoints/GetCustomerEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using Customers.Api.Contracts.Requests;
+using Customers.Api.Contracts.Responses;
+using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Customers.Api.Endpoints;
+
+[HttpGet("customers/{id:guid}"), AllowAnonymous]
+public class GetCustomerEndpoint : Endpoint<GetCustomerRequest, CustomerResponse>
+{
+    public override async Task HandleAsync(GetCustomerRequest req, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+}
