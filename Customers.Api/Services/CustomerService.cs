97f6b60910cb886ce08ea66f5fb027b73e2a3a84
Thomas Giesbrecht <thomas.giesbrecht@outlook.de>
2022-07-29 23:48:26 +0200

    Create customerService and add ICustomerRepo

---
+++ Customers.Api/Services/CustomerService.cs
@@ -0,0 +1,29 @@
+﻿using Customers.Api.Domain;
+using Customers.Api.Repositories;
+
+namespace Customers.Api.Services;
+
+public class CustomerService : ICustomerService
+{
+    private readonly ICustomerRepository _customerRepository;
+
+    public CustomerService(ICustomerRepository customerRepository)
+    {
+        _customerRepository = customerRepository;
+    }
+    
+    public Task<bool> CreateAsync(Customer customer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Customer?> GetAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Customer>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
