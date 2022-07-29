2f8935204ba95f7c0b9163545709628523a7d2b8
Thomas Giesbrecht <thomas.giesbrecht@outlook.de>
2022-07-29 23:57:12 +0200

    Call service in CreateCustomerEndpoint

--- Customers.Api/Endpoints/CreateCustomerEndpoint.cs
+++ Customers.Api/Endpoints/CreateCustomerEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿using Customers.Api.Contracts.Requests;
 using Customers.Api.Contracts.Responses;
 using Customers.Api.Mapping;
+using Customers.Api.Services;
 using FastEndpoints;
 using Microsoft.AspNetCore.Authorization;
 
@@ -9,14 +10,20 @@ namespace Customers.Api.Endpoints;
 [HttpPost("customers"), AllowAnonymous]
 public class CreateCustomerEndpoint : Endpoint<CreateCustomerRequest, CustomerResponse>
 {
+   private readonly ICustomerService _customerService;
+
+   public CreateCustomerEndpoint(ICustomerService customerService)
+   {
+      _customerService = customerService;
+   }
+   
    public override async Task HandleAsync(CreateCustomerRequest request, CancellationToken ct)
    {
       var customer = request.ToCustomer();
 
-      // await _customerService.CreateAsync(customer);
+      await _customerService.CreateAsync(customer);
 
       var customerResponse = customer.ToCustomerResponse();
-
       await SendCreatedAtAsync<GetCustomerEndpoint>(new { }, customerResponse, generateAbsoluteUrl: true, cancellation: ct);
    }
 }
